97c8cc98800261e23a8fa2a604f186e681e6a8c2
babybolsi <78638622+babybolsi@users.noreply.github.com>
2026-06-01 16:10:57 +0200

    Aggiunta ulteriori file per salvataggio dati

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <QApplication>
+#include <QWidget>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    return app.exec() ;
+
+    QWidget window;
+    window.setWindowTitle("Gestore Attività");
+    window.resize(800, 500);
+    window.show();
+
+    return app.exec();
 }
